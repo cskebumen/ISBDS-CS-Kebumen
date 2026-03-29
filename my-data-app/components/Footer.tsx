28cdd6bdc4dbfd7ebb239b813d3ace943ef1cb6e
cskebumen <isbdscskebumen96@gmail.com>
2026-03-29 19:23:48 +0000

    Update sistem manajemen data ISBDS

--- my-data-app/components/Footer.tsx
+++ my-data-app/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
   }, []);
 
   return (
-    <footer className="w-full py-6 mt-auto flex flex-col md:flex-row justify-between items-center px-4 md:px-8 bg-surface border-t border-slate-100 fixed bottom-0 left-0 right-0 md:left-64 z-30">
+    <footer className="w-full py-4 mt-auto flex flex-col md:flex-row justify-between items-center px-4 md:px-8 bg-surface border-t border-slate-80 fixed bottom-0 left-0 right-0 md:left-64 z-30">
       <p className="text-[7px] font-small uppercase tracking-widest text-slate-400">
         © {currentYear} ISBDS Cipta Sejati Cabang Kebumen. All rights reserved.
       </p>
